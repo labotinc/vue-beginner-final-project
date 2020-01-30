5fd064b4fab8c1dc11519aa7a3a11a7a9817694d
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2020-01-30 20:25:50 +0900

    Add warrior attack and fall back, implement damage to monster

--- src/assets/generateSprites.js
+++ src/assets/generateSprites.js
@@ -2,6 +2,15 @@ const fs = require("fs");
 const Spritesmith = require("spritesmith");
 
 // generate spritesheet
+const idle = [
+  __dirname + "/monster/ready_1.png",
+  __dirname + "/monster/ready_2.png",
+  __dirname + "/monster/ready_3.png",
+  __dirname + "/monster/ready_4.png",
+  __dirname + "/monster/ready_5.png",
+  __dirname + "/monster/ready_6.png"
+];
+
 const attack1 = [
   __dirname + "/warrior/attack1_1.png",
   __dirname + "/warrior/attack1_2.png",
@@ -9,6 +18,14 @@ const attack1 = [
   __dirname + "/warrior/attack1_4.png",
   __dirname + "/warrior/attack1_5.png",
   __dirname + "/warrior/attack1_6.png"
+];
+
+const jump = [
+  __dirname + "/warrior/jump_1.png",
+  __dirname + "/warrior/jump_2.png",
+  __dirname + "/warrior/jump_3.png",
+  __dirname + "/warrior/jump_4.png",
+  __dirname + "/warrior/jump_5.png",
 ];
 
 const attack2 = [
@@ -44,12 +61,12 @@ const stand = [
 
 const smith = new Spritesmith();
 
-smith.createImages(attack2, (error, images) => {
+smith.createImages(jump, (error, images) => {
   if (error) {
     throw error;
   }
 
   const result = smith.processImages(images, { algorithm: "left-right" });
 
-  result.image.pipe(fs.createWriteStream(__dirname + "/warrior/warrior_attack2_right.png"));
+  result.image.pipe(fs.createWriteStream(__dirname + "/warrior/warrior_jump.png"));
 });
