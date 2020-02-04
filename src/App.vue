7b33f1d3346960a82e64c86b87cc95a6baabed86
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2020-02-04 13:38:57 +0900

    Fix initial step

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
     <!-- TODO: 1. c. Use the imported Game component -->
-    <app-game></app-game>
   </div>
 </template>
 
 <script>
 // TODO: 1. a. Import the Game component
-import Game from "./Game";
 export default {
   name: "app",
   components: {
     // TODO: 1. b. Declare the Game component
-    'app-game': Game
   }
 };
 </script>
